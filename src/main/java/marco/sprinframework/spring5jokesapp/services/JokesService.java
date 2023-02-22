c18fb31b72a384428564dde9b359b0e5000f8b2d
Marco Hasler <marco.hasler@proton.me>
2023-02-22 20:02:54 +0100

    added jokes service

---
+++ src/main/java/marco/sprinframework/spring5jokesapp/services/JokesService.java
@@ -0,0 +1,6 @@
+package marco.sprinframework.spring5jokesapp.services;
+
+public interface JokesService {
+
+    String getJoke();
+}
---
+++ src/main/java/marco/sprinframework/spring5jokesapp/services/JokesServiceImpl.java
@@ -0,0 +1,8 @@
+package marco.sprinframework.spring5jokesapp.services;
+
+public class JokesServiceImpl implements JokesService {
+    @Override
+    public String getJoke() {
+        return null;
+    }
+}
