64358e7f25443cd8af1974eff8db91733b7a93dd
Marco Hasler <marco.hasler@proton.me>
2023-02-22 20:03:41 +0100

    added jokes service

--- src/main/java/marco/sprinframework/spring5jokesapp/services/JokesServiceImpl.java
+++ src/main/java/marco/sprinframework/spring5jokesapp/services/JokesServiceImpl.java
@@ -1,5 +1,8 @@
 package marco.sprinframework.spring5jokesapp.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class JokesServiceImpl implements JokesService {
     @Override
     public String getJoke() {
