d68f8aba616ed28482e7d593b4fc5483ce9d9586
Marco Hasler <marco.hasler@proton.me>
2023-02-22 20:22:10 +0100

    added JokeController

---
+++ src/main/java/marco/sprinframework/spring5jokesapp/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package marco.sprinframework.spring5jokesapp.controllers;
+
+import marco.sprinframework.spring5jokesapp.services.JokesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokesService jokesService;
+
+    public JokeController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokesService.getJoke());
+
+        return "index";
+    }
+}
